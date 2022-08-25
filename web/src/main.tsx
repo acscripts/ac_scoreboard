2c42d558f6033c70c14bd77ebbdc19f63d5dace7
ANTOND <71350868+antond15@users.noreply.github.com>
2022-08-25 15:16:22 +0200

    feat(nui): Add boilerplate files

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { VisibilityProvider } from './providers/VisibilityProvider';
 
-import './index.scss'
+import './index.css'
 import App from './App'
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <App />
+    <VisibilityProvider>
+      <App />
+    </VisibilityProvider>
   </StrictMode>
 );

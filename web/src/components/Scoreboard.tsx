19bbbc3d75d11a7ed5353e1a8fb6709111f4e78f
ANTOND <71350868+antond15@users.noreply.github.com>
2022-08-25 19:51:58 +0200

    refactor(web): Mock data

--- web/src/components/Scoreboard.tsx
+++ web/src/components/Scoreboard.tsx
@@ -8,56 +8,60 @@ import {
   DrawerContent,
   Text,
   Button,
+  Icon,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { FaUserFriends } from "react-icons/fa";
 import { useNuiEvent } from "../hooks/useNuiEvent";
 import { isEnvBrowser } from "../utils/misc";
 import { debugData } from "../utils/debugData";
 
 interface Props {
+  serverName: string;
   playerCount: number;
   maxPlayers: number;
 }
 
+const mockData: Props = {
+  serverName: "Server Name",
+  playerCount: 20,
+  maxPlayers: 64,
+};
+
 debugData([
   {
     action: "setData",
-    data: {
-      playerCount: 20,
-      maxPlayers: 64,
-    },
+    data: mockData,
   },
 ]);
 
 const Scoreboard: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [playerCount, setPlayerCount] = useState(0);
-  const [maxPlayers, setMaxPlayers] = useState(0);
+  const [data, setData] = useState<Props>(mockData);
 
   useNuiEvent("setVisible", onOpen);
-
-  useNuiEvent<Props>("setData", (data) => {
-    setPlayerCount(data.playerCount);
-    setMaxPlayers(data.maxPlayers);
-  });
+  useNuiEvent<Props>("setData", setData);
 
   return (
     <>
       {isEnvBrowser() && (
-        <Button colorScheme='blue' onClick={onOpen}>Open</Button>
+        <Button colorScheme="blue" onClick={onOpen}>
+          Open
+        </Button>
       )}
       <Drawer isOpen={isOpen} onClose={onClose} placement="right">
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerHeader>Some header</DrawerHeader>
+          <DrawerHeader>{data.serverName}</DrawerHeader>
 
           <DrawerBody>
             <Text>Some content...</Text>
           </DrawerBody>
 
-          <DrawerFooter>
+          <DrawerFooter justifyContent="center">
+            <Icon as={FaUserFriends} boxSize={5} mr={2} />
             <Text>
-              {playerCount}/{maxPlayers}
+              {data.playerCount} / {data.maxPlayers}
             </Text>
           </DrawerFooter>
         </DrawerContent>

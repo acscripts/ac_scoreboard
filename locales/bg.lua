7ec9e8ce07ab44044e45863f5e8f43ab887fc195
Martinator9001 <90502234+Martinator9001@users.noreply.github.com>
2023-08-30 06:14:11 +0000

    Create bg.lua
    Bulgarian Translation

---
+++ locales/bg.lua
@@ -0,0 +1,12 @@
+return {
+	['ui_group'] = "Група",
+	['ui_count'] = "Брой",
+	['ui_name'] = "Име",
+	['ui_id'] = "ЕГН",
+	['ui_player_count'] = "Брой на играчите",
+	['ui_your_id'] = "Твоето ЕГН",
+	['ui_copied'] = "Копирано!",
+
+	['command_open'] = "Отваря скорборда",
+	['keymap_open'] = "Oтвори скорборда",
+}

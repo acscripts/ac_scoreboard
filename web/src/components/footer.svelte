ba0a78e1e2c3eebbfd4e601dd6dc300e0a315ee0
ANTOND. <71350868+antond15@users.noreply.github.com>
2024-12-08 16:34:43 +0100

    fix(web/footer): preload tooltip icons

--- web/src/components/footer.svelte
+++ web/src/components/footer.svelte
@@ -3,7 +3,7 @@
   import locales from '$store/locales';
   import { fetchNui } from '$utils/fetchNui';
   import { cn } from '$utils/misc';
-  import Icon from '@iconify/svelte';
+  import Icon, { loadIcons } from '@iconify/svelte';
   import * as Tooltip from './lib/tooltip';
 
   let hasCopied = false;
@@ -13,6 +13,13 @@
     hasCopied = true;
     setTimeout(() => (hasCopied = false), 4000);
   };
+
+  const copyIcons = {
+    copy: 'mdi:cursor-default-click',
+    copied: 'mdi:clipboard-check',
+  };
+
+  loadIcons([copyIcons.copy, copyIcons.copied]);
 </script>
 
 {#if $data.footer}
@@ -26,7 +33,7 @@
       </Tooltip.Trigger>
       <Tooltip.Content class={cn(hasCopied && 'bg-green-600')}>
         <div class="flex items-center gap-1">
-          <Icon icon={hasCopied ? 'mdi:clipboard-check' : 'mdi:cursor-default-click'} class="h-4 w-4" />
+          <Icon icon={hasCopied ? copyIcons.copied : copyIcons.copy} class="h-4 w-4" />
           <p>{hasCopied ? $locales.server_id_copied : $locales.copy_server_id}</p>
         </div>
       </Tooltip.Content>
